039e0f11d321405ffe830b6cdef63a62a46bbc5e
Zelda789 <Zelda789>
2016-11-23 17:27:39 +0700

    blog_native

---
+++ sidebar.php
@@ -0,0 +1,41 @@
+<section id="sidebar">
+    <div class="module">
+        <h3 class="module-title">
+            About Me
+        </h3>
+        <div class="module-content">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos veniam id fugit, voluptatum maxime a laborum possimus, obcaecati, quia molestias accusantium consequuntur qui ullam? Nesciunt amet debitis alias aspernatur minus!
+            </p>
+        </div>
+    </div>
+    <div class="module">
+        <h3 class="module-title">
+            Latest News
+        </h3>
+        <div class="module-content">
+            <ul>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="module">
+        <h3 class="module-title">
+            Popular News
+        </h3>
+        <div class="module-content">
+            <ul>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+                <li><a href="">Lorem Ipsum</a></li>
+            </ul>
+        </div>
+    </div>
+</section>    </div>
+</section>
+<div class="clearfix"></div>
